54c868f8624699831cc0bd05b0a1d90d2f1a2905
tailgrab <tailgrabik@gmail.com>
2023-09-29 22:08:31 +0300

    1

--- trassir_exporter.py
+++ trassir_exporter.py
@@ -1,13 +1,21 @@
 import requests
 import yaml
 import prometheus_client
-# import urllib3
+import urllib3
+import json
+urllib3.disable_warnings()
 
-# urllib3.disable_warnings()
 
 def login():
-    payload={'username': 'prometheus', 'password':'1111'}
-    req=requests.get("https://192.168.1.80:8080/login",params=payload,verify=False)
+    payload={'username': 'prometheus', 'password':'CgkxGQaz5aOyR832'}
+    req=requests.get("https://192.168.2.80:8080/login",params=payload,verify=False)
+    res = json.loads(req.text)
+    if res['success'] == 1:
+        return res['sid']
+    else:
+        return False
+def get_data(comm,auth):
+    payload={'sid': auth}
+    req=requests.post(f'https://192.168.2.80:8080/{comm}',params=payload,verify=False)
     print(req.text)
-login()
-#get_data('health')
+get_data('health',login())
